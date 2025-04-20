47f99991240e4724d399cab58f1d35d3c4fd581a
Avinash <avinashkumarofficial95@gmail.com>
2025-04-20 20:23:45 +0530

    implemented bubble sort

--- 9.js
+++ 9.js
@@ -23,17 +23,44 @@
 
 
 // selection sort
-let min,temp,index,slicedArr;
-const arr = [5,12,22,41,12,1];
+// let min,temp,index,slicedArr;
+// const arr = [5,12,22,41,12,1];
+// console.log('arr',arr);
+// for(let i=0;i<arr.length-1;i++){
+//     slicedArr = arr.slice(i);    
+//     min = Math.min(...slicedArr);    
+//     index = slicedArr.indexOf(min) + i;
+//     temp = arr[index];
+//     arr[index] = arr[i];
+//     arr[i] = temp;  
+// }
+// console.log('sortedarray',arr);
+
+
+/***
+ * 
+ * Revise above
+ */
+
+// Bubble sort:
+let n = Number(prompt("Enter the size of array"));
+const arr = new Array(n);
+
+for(let i=0;i<n;i++){
+    arr[i] = prompt('Enter a number');
+    if(arr[i] === null) break;
+    else arr[i] = Number(arr[i])
+}
+
 console.log('arr',arr);
-for(let i=0;i<arr.length-1;i++){
-    slicedArr = arr.slice(i);    
-    min = Math.min(...slicedArr);    
-    index = slicedArr.indexOf(min) + i;
-    temp = arr[index];
-    arr[index] = arr[i];
-    arr[i] = temp;  
+let temp;
+for(let i=0;i<n-1;i++){
+    for(let j=0;j<n-1-i;j++){
+        if(arr[j]<arr[j+1]){
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
 }
 console.log('sortedarray',arr);
-
-
