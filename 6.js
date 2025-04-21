690ecd28316e71d174ea683e10d053d7583caef6
Avinash <avinashkumarofficial95@gmail.com>
2025-04-21 19:20:53 +0530

    right rotation by k element

--- 6.js
+++ 6.js
@@ -294,19 +294,36 @@
 
 
 // left-rotation by k elements;
-const arr = [5,12,30,85,41];
-let k = Number(prompt("Enter a number"));
-console.log('arr',arr);
-k = k%arr.length;
-for(j=0;j<k;j++){
-    let copy = arr[0];
-for(let i=0;i<arr.length-1;i++){
-    arr[i] = arr[i+1];
-}
-arr[arr.length-1] = copy
-}
+// const arr = [5,12,30,85,41];
+// let k = Number(prompt("Enter a number"));
+// console.log('arr',arr);
+// k = k%arr.length;
+// for(j=0;j<k;j++){
+//     let copy = arr[0];
+// for(let i=0;i<arr.length-1;i++){
+//     arr[i] = arr[i+1];
+// }
+// arr[arr.length-1] = copy
+// }
 
-console.log('leftrotationbyk',arr);
+// console.log('leftrotationbyk',arr);
+
+
+
+// right rotation by k element;
+// const arr=[1,2,3,4,5,6];
+// let k = Number(prompt('Enter k value'));
+// k = k%arr.length;
+// console.log('arr',arr);
+
+// for(j=0;j<k;j++){
+//     let copy = arr[arr.length-1]
+// for(let i=arr.length-1;i>0;i--){
+//     arr[i] = arr[i-1];
+// }
+// arr[0] = copy
+// }
+// console.log('rightrotationbyk',arr);
 
 
 
