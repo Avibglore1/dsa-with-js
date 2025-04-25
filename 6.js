1e0cc47be0da346c85ae3fcce73654554c0e6ea5
Avinash <avinashkumarofficial95@gmail.com>
2025-04-25 22:45:54 +0530

    removed duplicated from sorted array

--- 6.js
+++ 6.js
@@ -654,3 +654,16 @@
 //     }
 // }
 // console.log('arr',arr);
+
+// remove duplicates from array
+const arr = [1,2,5,5,8,9,9];
+console.log('arr',arr);
+
+let j=0;
+for(let i=1;i<arr.length;i++){
+    if(arr[i]!==arr[j]){
+       j++;
+       arr[j] = arr[i]
+    }
+}
+console.log('arr',arr.slice(0,j+1));
