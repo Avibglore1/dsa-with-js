eda9d9417f40420dec690d21b61c05985a5cf661
Avinash <avinashkumarofficial95@gmail.com>
2025-04-29 12:02:57 +0530

    sorted and rotatedarray checker

--- dsa_cheatsheet/step3.js
+++ dsa_cheatsheet/step3.js
@@ -8,20 +8,34 @@
 // console.log('maximum',max);
 
 
-let arr = [2,23,45,65,365,45];
-if(arr.length<2) console.log(-1);
-else{
-     let max = arr[0];
-let smax = -Infinity;
+// let arr = [2,23,45,65,365,45];
+// if(arr.length<2) console.log(-1);
+// else{
+//      let max = arr[0];
+// let smax = -Infinity;
+// for(let i=0;i<arr.length;i++){
+//      if(arr[i]>max){
+//           smax = max;
+//           max = arr[i];
+//      }else if(arr[i]>smax && arr[i]<max){
+//           smax = arr[i];
+//      }
+// }
+// if(smax === -Infinity) console.log(-1);
+// else console.log('secondmaximum',smax);
+// }
+
+
+// initiallysortedandrotated:
+const arr = [1,2,3];
+let count=0;
+
 for(let i=0;i<arr.length;i++){
-     if(arr[i]>max){
-          smax = max;
-          max = arr[i];
-     }else if(arr[i]>smax && arr[i]<max){
-          smax = arr[i];
+     if(arr[i]<arr[(i+1)%arr.length]){
+          count++;
      }
 }
-if(smax === -Infinity) console.log(-1);
-else console.log('secondmaximum',smax);
-}
+if(count<=1) console.log(true);
+else console.log(false);
+
 
