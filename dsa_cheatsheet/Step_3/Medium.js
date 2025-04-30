01ed8dddf05918dd67e5d1ec66f0eb9628266894
Avinash <avinashkumarofficial95@gmail.com>
2025-04-30 10:28:46 +0530

    maximum profit in aweek

--- dsa_cheatsheet/Step_3/Medium.js
+++ dsa_cheatsheet/Step_3/Medium.js
@@ -18,13 +18,25 @@
 // console.log('majorityItem',majorityItem);
 
 
-// Kadane's Algorithm:
-const nums = [2, 3, 5, -2, 7, -4];
-let sum=0,maxSum=-Infinity;
+// Kadane's Algorithm:maximum sum in a subarray
+// const nums = [2, 3, 5, -2, 7, -4];
+// let sum=0,maxSum=-Infinity;
+// for(let i=0;i<nums.length;i++){
+//     sum += nums[i];
+//     if(sum>maxSum) maxSum = sum;
+//     if(sum<0) sum = 0;
+// }
+// console.log('maxsum',maxSum);
+
+
+// MaximumProfit:
+const nums = [7,1,5,3,6,4];
+let minPrice = Infinity;
+let MaximumProfit = -Infinity;
 for(let i=0;i<nums.length;i++){
-    sum += nums[i];
-    if(sum>maxSum) maxSum = sum;
-    if(sum<0) sum = 0;
+    if(nums[i]<minPrice) minPrice = nums[i];
+    if(nums[i]-minPrice>MaximumProfit) MaximumProfit = nums[i]-minPrice;
 }
-console.log('maxsum',maxSum);
+console.log('maxProfit', MaximumProfit);
+
 
