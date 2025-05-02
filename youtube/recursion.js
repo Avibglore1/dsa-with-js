c89da4ad63cc4a07a9842fc2b818738faf4377c9
Avinash <avinashkumarofficial95@gmail.com>
2025-05-02 19:44:29 +0530

    print from 1 to n

--- youtube/recursion.js
+++ youtube/recursion.js
@@ -1,14 +1,29 @@
 // print n to 1;
 
 
-let start = 1,end=10
+// let start = 1,end=10
+// function print(start,end){
+//     if(start===end){
+//         console.log(end);
+//         return
+//     }
+// console.log(end);
+// print(start,end-1)
+// }
+
+// print(start,end);
+
+// from 1 to n:
+
+let start = 1;
+let end = 10;
 function print(start,end){
     if(start===end){
-        console.log(end);
+        console.log(start);
         return
     }
-console.log(end);
-print(start,end-1)
+    console.log(start);
+    print(start+1,end)   
 }
 
-print(start,end);
+print(start,end)
