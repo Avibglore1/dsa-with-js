c1f096d6c87128acd0639c536b07850dbbf4b1bb
Avinash <avinashkumarofficial95@gmail.com>
2025-05-02 21:21:29 +0530

    fibonacci series sum

--- youtube/recursion.js
+++ youtube/recursion.js
@@ -15,15 +15,49 @@
 
 // from 1 to n:
 
-let start = 1;
-let end = 10;
-function print(start,end){
-    if(start===end){
-        console.log(start);
-        return
-    }
-    console.log(start);
-    print(start+1,end)   
-}
+// let start = 1;
+// let end = 10;
+// function print(start,end){
+//     if(start===end){
+//         console.log(start);
+//         return
+//     }
+//     console.log(start);
+//     print(start+1,end)   
+// }
 
-print(start,end)
+// print(start,end)
+
+
+// function fn(n){
+//     if(n===0) return;
+//     fn(n-1);
+//     console.log(n);
+// }
+
+// fn(5);
+
+
+// let n=10;
+// let first=0,second = 1;
+// console.log(first, second);
+// for(let i=1;i<=n-2;i++){
+//     fn = first + second;
+//     console.log(fn);    
+//     first = second;
+//     second = fn;
+// }
+
+
+let n=10;
+let first = 0,second = 1;
+function fn(n,first,second){
+    if(n===0) return ;
+    third = first + second;
+    console.log(third);
+     first = second;
+     second = third,
+     fn(n-1,first,second)
+   
+}
+fn(n,first,second)
