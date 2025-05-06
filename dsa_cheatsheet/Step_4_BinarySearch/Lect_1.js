efb10eede4cb4d33ea15fdb0001040a4b74d474b
Avinash <avinashkumarofficial95@gmail.com>
2025-05-06 09:57:01 +0530

    lower bound in a sorted array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
@@ -1,15 +1,32 @@
-const nums= [8,15,25,56,890];
-let target = 56;
-let first = 0,last=nums.length-1,mid;
-let flag = false;
+// const nums= [8,15,25,56,890];
+// let target = 56;
+// let first = 0,last=nums.length-1,mid;
+// let flag = false;
+// while(first<=last){
+//     mid = Math.floor((first+last)/2);
+//     if(nums[mid]===target) {
+//         flag = true;
+//         console.log(mid);
+//         break;
+//     }
+//     else if (nums[mid]>target) last = mid-1;
+//     else first = mid+1;
+// }
+// if(!flag) console.log('Target element not found');
+
+
+
+// Lower Bound:
+const nums = [8,15,25,56,890];
+const target = 28;
+let first = 0,last = nums.length-1,mid;
+let ans = nums.length;
 while(first<=last){
     mid = Math.floor((first+last)/2);
-    if(nums[mid]===target) {
-        flag = true;
-        console.log(mid);
-        break;
-    }
-    else if (nums[mid]>target) last = mid-1;
-    else first = mid+1;
+    if(nums[mid]>=target){
+        ans = mid;
+        last = mid-1;
+    }else first = mid+1;
 }
-if(!flag) console.log('Target element not found');
+console.log(ans);
+
