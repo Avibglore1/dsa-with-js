3df17c17865c6dc6addad74f3d0dd6e1828089a5
Avinash <avinashkumarofficial95@gmail.com>
2025-05-06 18:00:29 +0530

    seach in a rotated array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
@@ -89,27 +89,49 @@
 
 
     // Find First and Last Position of Element in Sorted Array:
-    const nums = [8,15,25,56,890];
-    const target = 28;
-    let firstIndex,lastIndex;
-    function find_index(is_first){
-        let left=0,right=nums.length-1,mid;
-        let result = -1;
+    // const nums = [8,15,25,56,890];
+    // const target = 28;
+    // let firstIndex,lastIndex;
+    // function find_index(is_first){
+    //     let left=0,right=nums.length-1,mid;
+    //     let result = -1;
+    //     while(left<=right){
+    //         mid = Math.floor((left+right)/2);
+    //         if(nums[mid]===target){
+    //             result = mid;
+    //             if(is_first) right=mid-1;
+    //             else left = mid+1;
+    //         }
+    //         else if(nums[mid]>target) right = mid-1;
+    //         else left = mid+1;
+    //     }
+    //     return result;
+    // }
+    // firstIndex = find_index(true);
+    // lastIndex = find_index(false);
+    // console.log([firstIndex,lastIndex]);
+
+    
+    // Search in Rotated Sorted Array:
+     const nums = [4,5,6,7,0,1,2];
+    const target = 0;
+    function search(){
+        let left=0,right = nums.length-1,mid;
         while(left<=right){
             mid = Math.floor((left+right)/2);
-            if(nums[mid]===target){
-                result = mid;
-                if(is_first) right=mid-1;
+            if(nums[mid]===target) return mid;
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target < nums[mid]) right = mid-1;
                 else left = mid+1;
+            }else{
+                if(nums[mid]<target && target<=nums[right]) left=mid+1;
+                else right = mid-1;
             }
-            else if(nums[mid]>target) right = mid-1;
-            else left = mid+1;
         }
-        return result;
+        return -1;
     }
-    firstIndex = find_index(true);
-    lastIndex = find_index(false);
-    console.log([firstIndex,lastIndex]);
-    
-    
+console.log(search());
+
+
+  
 
