b697ddd5f0c88de748d2417345286d90dc47e8b0
Avinash <avinashkumarofficial95@gmail.com>
2025-05-06 11:55:45 +0530

    floor and ceil of a target element in a sorted array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
@@ -47,19 +47,44 @@
 
 
     // Search insert position:
+    // const nums = [8,15,25,56,890];
+    // const target = 28;
+    // let first=0,last = nums.length-1,mid;
+    // let flag = false;
+    // while(first<=last){
+    //     mid = Math.floor((first+last)/2);
+    //     if(nums[mid]===target) {
+    //         flag = true;
+    //         console.log(mid);
+    //         break;
+    //     }
+    //     else if(nums[mid]>target) last = mid-1;
+    //     else first = mid+1;
+    // }
+    // if(!flag) console.log(first);
+    
+
+    // Floor and Ceil in Sorted Array:
     const nums = [8,15,25,56,890];
     const target = 28;
+    let floor=-1,ceil=-1;
     let first=0,last = nums.length-1,mid;
-    let flag = false;
-    while(first<=last){
-        mid = Math.floor((first+last)/2);
-        if(nums[mid]===target) {
-            flag = true;
-            console.log(mid);
-            break;
+    function floorCeil(){
+        while(first<=last){
+            mid = Math.floor((first+last)/2);
+            if(nums[mid]===target) return [target,target];
+            else if(nums[mid]>target){
+                ceil = nums[mid];
+                last = mid-1;
+            }else{
+                floor = nums[mid];
+                first = mid+1;
+            }
         }
-        else if(nums[mid]>target) last = mid-1;
-        else first = mid+1;
+        return [floor,ceil]
     }
-    if(!flag) console.log(first);
+   
+    const ans = floorCeil();
+    console.log(ans);
     
+
