8a0b622ca29b5c8047f906581dad16b255062bf6
Avinash <avinashkumarofficial95@gmail.com>
2025-05-06 08:53:02 +0530

    restrured files and folders

--- youtube/BinarySearch.js
+++ youtube/BinarySearch.js
@@ -20,23 +20,7 @@
 // else console.log('Search element available at index', index);
 
 
-/**************question and solution******************* */
-//  Binary Search:
-const nums= [8,15,25,56,890];
-let target = 56;
-let first = 0,last=nums.length-1,mid;
-let flag = false;
-while(first<=last){
-    mid = Math.floor((first+last)/2);
-    if(nums[mid]===target) {
-        flag = true;
-        console.log(mid);
-        break;
-    }
-    else if (nums[mid]>target) last = mid-1;
-    else first = mid+1;
-}
-if(!flag) console.log('Target element not found');
+
 
 
 
