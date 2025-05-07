a6459e07afcf0306822f23e47fc684caf6048645
Avinash <avinashkumarofficial95@gmail.com>
2025-05-07 19:56:02 +0530

    2d array search part-II

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_2.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_2.js
@@ -62,21 +62,39 @@
 
 
 // searching target element in a 2d array:
-const matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
-const target = 3;
-function twodArray(matrix,target){
-    const m = matrix.length;
-    const n = matrix[0].length;
-    let left=0,right = m*n-1;
-    while(left<=right){
-        let mid = Math.floor((left+right)/2);
-        let row = Math.floor(mid/n);
-        let col = mid%n;
-        let value = matrix[row][col];
-        if(value===target) return true;
-        else if(value<target) left=mid+1;
-        else right=mid-1;
-    }
-return false;
+// const matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
+// const target = 3;
+// function twodArray(matrix,target){
+//     const m = matrix.length;
+//     const n = matrix[0].length;
+//     let left=0,right = m*n-1;
+//     while(left<=right){
+//         let mid = Math.floor((left+right)/2);
+//         let row = Math.floor(mid/n);
+//         let col = mid%n;
+//         let value = matrix[row][col];
+//         if(value===target) return true;
+//         else if(value<target) left=mid+1;
+//         else right=mid-1;
+//     }
+// return false;
+// }
+// console.log(twodArray(matrix,target))
+
+
+// searching in a 2d array both row and col are arrange in ascending order:
+const matrix=[ [1, 4, 7, 11, 15], [2, 5, 8, 12, 19], [3, 6, 9, 16, 22], [10, 13, 14, 17, 24], [18, 21, 23, 26, 30] ];
+const target = 20;
+function search(matrix,target){
+let m = matrix.length;
+let n= matrix[0].length;
+let row=0,col=n-1;
+while(row<m && col>=0){
+    let value = matrix[row][col];
+    if (value===target) return true;
+    else if(value>target) col--;
+    else row++
 }
-console.log(twodArray(matrix,target))
+return false
+}
+console.log(search(matrix,target))
