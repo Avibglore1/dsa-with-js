4c67bbc71aa50d86c8423c417687a00b6b63b8b6
Avinash <avinashkumarofficial95@gmail.com>
2025-05-07 18:30:35 +0530

    binary search in a 2d array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_2.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_2.js
@@ -43,18 +43,40 @@
 
 // Kth Missing Positive Number:
 // TimeComplexity:O(logn)
-const arr = [2,3,4,7,11];
-const k=5;
+// const arr = [2,3,4,7,11];
+// const k=5;
 
-function missing_binarySearch(nums,k){
-let left=0,right=nums.length,mid,missing;
-while(left<right){
-    mid = Math.floor((left+right)/2);
-    missing = nums[mid]-(mid+1);
-    if(missing<k) left=mid+1;
-    else right=mid;
-}
-return left+k;
-}
+// function missing_binarySearch(nums,k){
+// let left=0,right=nums.length,mid,missing;
+// while(left<right){
+//     mid = Math.floor((left+right)/2);
+//     missing = nums[mid]-(mid+1);
+//     if(missing<k) left=mid+1;
+//     else right=mid;
+// }
+// return left+k;
+// }
 
-console.log(missing_binarySearch(arr,k));
+// console.log(missing_binarySearch(arr,k));
+
+
+
+// searching target element in a 2d array:
+const matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]];
+const target = 3;
+function twodArray(matrix,target){
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let left=0,right = m*n-1;
+    while(left<=right){
+        let mid = Math.floor((left+right)/2);
+        let row = Math.floor(mid/n);
+        let col = mid%n;
+        let value = matrix[row][col];
+        if(value===target) return true;
+        else if(value<target) left=mid+1;
+        else right=mid-1;
+    }
+return false;
+}
+console.log(twodArray(matrix,target))
