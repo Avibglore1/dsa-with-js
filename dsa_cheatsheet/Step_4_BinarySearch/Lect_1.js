424cd8ae7a4861e2e661110785666216e416155c
Avinash <avinashkumarofficial95@gmail.com>
2025-05-07 10:21:47 +0530

    minimum in distinct rotated sorted array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
@@ -134,26 +134,40 @@
 
 
 // Search in Rotated Sorted Array:with duplicates
-const nums = [2,5,6,0,0,1,2];
-const target = 3;
-  function search_In_Duplicates(){
-    let left = 0,right = nums.length-1,mid;
-    while(left<=right){
+// const nums = [2,5,6,0,0,1,2];
+// const target = 3;
+//   function search_In_Duplicates(){
+//     let left = 0,right = nums.length-1,mid;
+//     while(left<=right){
+//         mid = Math.floor((left+right)/2);
+//         if(nums[mid]===target) return true;
+//         if(nums[left]===nums[mid] && nums[mid]===nums[right]){
+//             left++;
+//             right--
+//         }else if(nums[left]<=nums[mid]){
+//             if(nums[left]<=target && target<nums[mid]) right = mid-1;
+//             else left = mid+1;
+//         }else{
+//             if(nums[mid]<target && target <= nums[right]) left = mid+1;
+//             else right = mid-1
+//         }
+//     }
+//     return false
+//   }
+
+//   console.log(search_In_Duplicates())
+
+
+
+// Find Minimum in Rotated Sorted Array:
+const nums= [11,13,15,17];
+function search(){
+    let left=0,right=nums.length-1,mid;
+    while(left<right){
         mid = Math.floor((left+right)/2);
-        if(nums[mid]===target) return true;
-        if(nums[left]===nums[mid] && nums[mid]===nums[right]){
-            left++;
-            right--
-        }else if(nums[left]<=nums[mid]){
-            if(nums[left]<=target && target<nums[mid]) right = mid-1;
-            else left = mid+1;
-        }else{
-            if(nums[mid]<target && target <= nums[right]) left = mid+1;
-            else right = mid-1
-        }
+        if(nums[mid]>nums[right]) left = mid+1;
+        else right = mid;
     }
-    return false
-  }
-
-  console.log(search_In_Duplicates())
-
+    return nums[left]
+}
+console.log(search());
