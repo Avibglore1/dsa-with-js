326b9afcda2a2829b6e25c8df2ae4f86f7086d7d
Avinash <avinashkumarofficial95@gmail.com>
2025-05-09 00:02:27 +0530

    reverse of an array

--- dsa_cheatsheet/Step_1_Learn_the_basics/Lect_5.js
+++ dsa_cheatsheet/Step_1_Learn_the_basics/Lect_5.js
@@ -1,9 +1,22 @@
 // Recursion
 // Factorial:
-const n=5;
-function factorial(n){
-    if(n==1) return 1;
-    else return n*factorial(n-1);
-}
+// const n=5;
+// function factorial(n){
+//     if(n==1) return 1;
+//     else return n*factorial(n-1);
+// }
 
-console.log(factorial(n));
+// console.log(factorial(n));
+
+
+const arr=[1,2,3,4,5];
+let n = 5;
+function reverse(arr,start,end){
+if(start>=end) return arr;
+let temp=arr[start];
+arr[start] = arr[end];
+arr[end] = temp;
+
+return reverse(arr,start+1,end-1)
+}
+console.log(reverse(arr,0,n-1))
