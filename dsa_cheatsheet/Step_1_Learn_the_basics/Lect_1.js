625951656e09b20b4194962f5584217dc9c29581
Avinash <avinashkumarofficial95@gmail.com>
2025-05-08 17:16:18 +0530

    implemented sieve of eratosthenes principle for collecting primes upto n

--- dsa_cheatsheet/Step_1_Learn_the_basics/Lect_1.js
+++ dsa_cheatsheet/Step_1_Learn_the_basics/Lect_1.js
@@ -205,6 +205,23 @@
 // else console.log('Not a Prime Number');
 
 
+// Sieve of eratoshthenese:
+const n=50;
+const isPrime = new Array(n+1).fill(true);
+isPrime[0]=isPrime[1]=false;
+for(let p=2;p*p<=n;p++){
+    if(isPrime[p]){
+        for(let i=p*p;i<=n;i+=p){
+            isPrime[i]=false;
+        }
+    }
+}
+const arr=[];
+for(let i=2;i<=n;i++){
+    if(isPrime[i]) arr.push(i);    
+}
+
+console.log(arr);
 
  
 
