bb2d6102996b9025abaa5e3e73d0db7190edf80f
Avinash <avinashkumarofficial95@gmail.com>
2025-05-09 10:50:22 +0530

    palindrome checker using recursion

--- dsa_cheatsheet/Step_1_Learn_the_basics/Lect_5.js
+++ dsa_cheatsheet/Step_1_Learn_the_basics/Lect_5.js
@@ -9,14 +9,28 @@
 // console.log(factorial(n));
 
 
-const arr=[1,2,3,4,5];
-let n = 5;
-function reverse(arr,start,end){
-if(start>=end) return arr;
-let temp=arr[start];
-arr[start] = arr[end];
-arr[end] = temp;
+// const arr=[1,2,3,4,5];
+// let n = 5;
+// function reverse(arr,start,end){
+// if(start>=end) return arr;
+// let temp=arr[start];
+// arr[start] = arr[end];
+// arr[end] = temp;
 
-return reverse(arr,start+1,end-1)
+// return reverse(arr,start+1,end-1)
+// }
+// console.log(reverse(arr,0,n-1))
+
+let s='Hannat';
+let sentence = s;
+s=s.split('');
+let start=0,end=s.length-1;
+function palindromeCheck(s,start,end){
+if(start>=end) return s;
+let temp=s[start];
+s[start] = s[end];
+s[end] = temp;
+return palindromeCheck(s,start+1,end-1)
 }
-console.log(reverse(arr,0,n-1))
+const ans = palindromeCheck(s,start,end);
+console.log(ans.join('').toUpperCase()===sentence.toUpperCase());
