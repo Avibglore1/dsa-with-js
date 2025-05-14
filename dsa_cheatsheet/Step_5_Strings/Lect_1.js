9263154e45d0262e5a920d4200ef013676d22fa8
Avinash <avinashkumarofficial95@gmail.com>
2025-05-14 19:05:10 +0530

    reversing a word

--- dsa_cheatsheet/Step_5_Strings/Lect_1.js
+++ dsa_cheatsheet/Step_5_Strings/Lect_1.js
@@ -1,16 +1,24 @@
 // Remove outermost parenthesis:
-const s="()()";
-let result='';
-let count=0;
-for(let char of s){
-    if(char==='('){
-        if(count>0) result +=char;
-        count++;
-    }
-    if(char===')'){
-        count--;
-        if(count>0) result +=char;
-    }
-}
+// const s="()()";
+// let result='';
+// let count=0;
+// for(let char of s){
+//     if(char==='('){
+//         if(count>0) result +=char;
+//         count++;
+//     }
+//     if(char===')'){
+//         count--;
+//         if(count>0) result +=char;
+//     }
+// }
 
-console.log(result);
+// console.log(result);
+
+
+// reverse a word haaving whitespaces in leading/trailing and spaces within word:
+let s="a good   example";
+
+s=s.trim().split(/\s+/).reverse().join(' ');
+console.log(s);
+
