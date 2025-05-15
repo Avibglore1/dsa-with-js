7bcea4fcb45bb2bab6021de8dbcaca5dcd2cdc96
Avinash <avinashkumarofficial95@gmail.com>
2025-05-15 12:40:28 +0530

    isomorphic strong

--- dsa_cheatsheet/Step_5_Strings/Lect_1.js
+++ dsa_cheatsheet/Step_5_Strings/Lect_1.js
@@ -17,8 +17,29 @@
 
 
 // reverse a word haaving whitespaces in leading/trailing and spaces within word:
-let s="a good   example";
+// let s="a good   example";
 
-s=s.trim().split(/\s+/).reverse().join(' ');
-console.log(s);
+// s=s.trim().split(/\s+/).reverse().join(' ');
+// console.log(s);
 
+
+// Isomorphic String:
+const s='egg';
+const t='add';
+
+function isomorphic(s,t){
+if(s.length!==t.length) return false;
+let sObj={},tObj={};
+for(let i=0;i<s.length;i++){
+    let schar=s[i];
+    let tchar=t[i];
+
+    if(sObj[schar] && sObj[schar]!==tchar) return false;
+    if(tObj[tchar] && tObj[tchar]!==schar) return false;
+
+    sObj[schar] = tchar;
+    tObj[tchar] = schar;
+}
+return true;
+}
+console.log(isomorphic(s,t));
