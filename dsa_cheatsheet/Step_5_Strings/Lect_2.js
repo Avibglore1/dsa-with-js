edf290c97bd7cdf9b20f9c4546faf5902cd24d75
Avinash <avinashkumarofficial95@gmail.com>
2025-05-16 08:50:54 +0530

    string to integer-atoi function

--- dsa_cheatsheet/Step_5_Strings/Lect_2.js
+++ dsa_cheatsheet/Step_5_Strings/Lect_2.js
@@ -1,11 +1,41 @@
 // Depth of nesting:
-const s= "()(())((()()))";
-let count=0,maxCount=0;
-for(let char of s){
-    if(char==='('){
-        count++;
-        if(count>maxCount) maxCount=count;
+// const s= "()(())((()()))";
+// let count=0,maxCount=0;
+// for(let char of s){
+//     if(char==='('){
+//         count++;
+//         if(count>maxCount) maxCount=count;
+//     }
+//     else if(char===')') count--;
+// }
+// console.log(maxCount);
+
+// Atoi function:
+
+let s="-91283472332"
+ function atoi(s){
+let result='';
+    let newString;
+    if(s.length===0) return 0;
+    let trimmed = s.trim();
+  let sign= trimmed[0];
+  if(sign==='-'|| sign=== "+") newString = trimmed.slice(1); 
+  else{
+    newString = trimmed;
+  }
+    for(let char of newString){
+        if(char<'0' || char>'9') break;
+        result +=char
     }
-    else if(char===')') count--;
-}
-console.log(maxCount);
+    if(result==='') return 0;
+    let temp = Number(result);
+   
+    if (sign==='-'){
+        if(temp>Math.pow(2,31)) temp=Math.pow(2,31);
+        return -temp;
+    }
+    if(temp>Math.pow(2,31)-1) temp=Math.pow(2,31);
+    return temp;
+ }
+ 
+ console.log(atoi(s));
