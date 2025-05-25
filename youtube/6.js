1329f655db8a0b7601dfc2a59738fd523b951288
Avinash <avinashkumarofficial95@gmail.com>
2025-05-25 12:52:44 +0530

    left rotation by 1 element

--- youtube/6.js
+++ youtube/6.js
@@ -765,3 +765,15 @@
 
 // if(count>0) console.log(item);
 
+
+
+/********revision*********************revision*******************************revision**********/
+
+const arr=[1,2,3,4,5]
+let copy=arr[0];
+for(let i=0;i<arr.length-1;i++){
+    arr[i]=arr[i+1];
+}
+arr[arr.length-1]=copy;
+console.log(arr);
+
