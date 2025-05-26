e559fa9c8ee4383fc1263ded505c2fc0ccd083e8
Avinash <avinashkumarofficial95@gmail.com>
2025-05-26 19:27:05 +0530

    bubble sort implementation

--- dsa_cheatsheet/Step_2_Sorting/Lect_2.js
+++ dsa_cheatsheet/Step_2_Sorting/Lect_2.js
@@ -32,33 +32,46 @@
 
 // Quick Sort: O(nlogn) worse case:O(n^2),with no extra space
 
-function pivotIndexFn(arr,first,last){
-let pivot = arr[first];
-let i=first+1; 
-let j=last;
-while(i<=j){
-    while(i<=last && arr[i]<=pivot) i++;
-    while(j>first && arr[j]>=pivot) j--;
-    if(i<j) swap(arr,i,j)
-}
-swap(arr,j,first);
-return j
-}
+// function pivotIndexFn(arr,first,last){
+// let pivot = arr[first];
+// let i=first+1; 
+// let j=last;
+// while(i<=j){
+//     while(i<=last && arr[i]<=pivot) i++;
+//     while(j>first && arr[j]>=pivot) j--;
+//     if(i<j) swap(arr,i,j)
+// }
+// swap(arr,j,first);
+// return j
+// }
 
-function quickSort(nums,first,last){
-    if(first>=last) return;
-    let pivotIndex = pivotIndexFn(nums,first,last);
-    quickSort(nums,first,pivotIndex-1);
-    quickSort(nums,pivotIndex+1,last)
+// function quickSort(nums,first,last){
+//     if(first>=last) return;
+//     let pivotIndex = pivotIndexFn(nums,first,last);
+//     quickSort(nums,first,pivotIndex-1);
+//     quickSort(nums,pivotIndex+1,last)
+// }
+
+// function swap(arr,i,j){
+// let temp = arr[i];
+// arr[i] = arr[j];
+// arr[j] = temp;
+// }
+
+// const nums = [7, 4, 1, 5, 3];
+// quickSort(nums,0,nums.length-1);
+// console.log(nums);
+
+
+// Bubble Sorting:
+const arr=[10,1,5,12,3];
+
+for(let i=0;i<arr.length-1;i++){
+    for(let j=0;j<arr.length-i-1;j++){
+        if(arr[j]>arr[j+1]){
+            [arr[j],arr[j+1]] = [arr[j+1],arr[j]];
+        }
+    }
 }
-
-function swap(arr,i,j){
-let temp = arr[i];
-arr[i] = arr[j];
-arr[j] = temp;
-}
-
-const nums = [7, 4, 1, 5, 3];
-quickSort(nums,0,nums.length-1);
-console.log(nums);
-
+console.log(arr);
+// bubble element is passed to next if condition is not met:
