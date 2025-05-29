906e4e6083b0163ff580e2ac0027e5f2dedfd49a
Avinash <avinashkumarofficial95@gmail.com>
2025-05-29 19:07:20 +0530

    counting uniqueElements in an array

--- dsa_cheatsheet/Step_3_Arrays/Easy.js
+++ dsa_cheatsheet/Step_3_Arrays/Easy.js
@@ -213,16 +213,29 @@
 
 
 // check if array is sorted and rotated:
-const nums = [2,3,4,5];
-let count=0;
-for(let i=0;i<nums.length;i++){
-    if(nums[i]>nums[(i+1)%nums.length]) count++;
+// const nums = [2,3,4,5];
+// let count=0;
+// for(let i=0;i<nums.length;i++){
+//     if(nums[i]>nums[(i+1)%nums.length]) count++;
+// }
+// if(count<=1) {
+//     if(nums[0]>nums[nums.length-1]) console.log('Array is sorted and rotated');
+//     else console.log('Array is sorted');   
+// } 
+// else console.log('Array was not sorted initially');
+
+
+// Remove duplicates from sorted Array:
+const nums=[0,0,1,1,1,2,2,3,3,4];
+let j=0;
+for(let i=1;i<nums.length;i++){
+    if(nums[i]!==nums[j]){
+        j++;
+        nums[j]=nums[i];
+    }
 }
-if(count<=1) {
-    if(nums[0]>nums[nums.length-1]) console.log('Array is sorted and rotated');
-    else console.log('Array is sorted');   
-} 
-else console.log('Array was not sorted initially');
+console.log('uniqueElements',j+1);
+
 
 
 
