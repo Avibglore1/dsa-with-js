4267c373a4bde031218038ffc88d645e11cb5de2
Avinash <avinashkumarofficial95@gmail.com>
2025-06-07 14:44:40 +0530

    search / insert target element in a sorted array

--- dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
+++ dsa_cheatsheet/Step_4_BinarySearch/Lect_1.js
@@ -325,4 +325,21 @@
 // }
 // console.log(occurence([1, 2, 3, 4, 5], 6));
 
+// Search Insert Element:
+
+const nums=[1,3,5,6];
+const target=2;
+function searchInsert(nums,target){
+    let left=0,right=nums.length-1;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        if(nums[mid]===target) return mid
+        if(nums[mid]<target){
+            left=mid+1;
+        }else right=mid-1
+}
+return left
+}
+console.log(searchInsert(nums,target));
+
 
